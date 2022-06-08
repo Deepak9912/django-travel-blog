c37a97b0db991c705fc7cbf211db36ceaa89a95a
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-08 14:43:18 +0000

    update realted name for post and comment so that they are valid python idetifier

--- blog/models.py
+++ blog/models.py
@@ -7,15 +7,15 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.foreignKey(
-        User, on_delete=models.CASCADE, related_name="blog-posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured-image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManytoManyField(User, related_name="blog-likes", blank=True)
+    likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -28,8 +28,8 @@ class Post(models.Model):
 
 
 class comment(models.Model):
-    post = models.foreignKey(
-        Post, on_delete=models.CASCADE, related_name="blog-comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="blog_comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
