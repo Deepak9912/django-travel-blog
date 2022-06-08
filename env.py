eb319a0830fe6121dcfe1ba74dae32c7f6a6195b
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-08 10:03:41 +0000

    deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://vvrflbvsmqkxlx:b0cfbc68cbf46aa43e2ccef84ba395fa3e20a993425bb37a14512d079bb320c8@ec2-99-81-137-11.eu-west-1.compute.amazonaws.com:5432/d6s5rdgvj909rc"
+os.environ["SECRET_KEY"] = "randomSecretKey2022!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://453846692468495:kooGBDhSAPtmvGcWQdl2wt24_OY@django22-blog-student"
