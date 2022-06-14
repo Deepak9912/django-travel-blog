6011dd6d20c89e6e94befb474a2715f629172b07
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-14 14:51:19 +0000

    add new class view LikeBlog and import HttpresponseRedirect and reverse

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -34,6 +35,7 @@ class DetailBlog(View):
         )
     
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -41,16 +43,15 @@ class DetailBlog(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        user_comment = CommentForm(data=request.POST)
-
-        if user_comment.is_valid():
-            user_comment.instance.email = request.user.email
-            user_comment.instance.name = request.user.username
-            comment = user_comment.save(commit=False)
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
         else:
-            user_comment = CommentForm() 
+            comment_form = CommentForm()
 
         return render(
             request,
@@ -63,4 +64,15 @@ class DetailBlog(View):
                 "liked": liked
             },
         )
- 
+
+class LikeBlog(View):
+
+    def post(self, request, slug):
+
+        post = get_object_or_404(queryset, slug=slug)
+        if post.likes.filter(id=self.request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('detail_blog', args=[slug]))
