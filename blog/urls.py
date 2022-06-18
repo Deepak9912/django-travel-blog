9acf1c1478870355505ea6e04cdee15d1fed88c8
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-18 19:51:14 +0000

    update url pattern for new contact form

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from .views import ContactFormView
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.DetailBlog.as_view(), name='detail_blog'),
-    path('', ContactFormView.as_view(), name='contact_form'),
+    path('', views.ContactFormView.as_view(), name='contact_form'),
 ]
