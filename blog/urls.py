cee5b4598e69b5ad116f875dcd6815a77b9c2370
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-20 18:58:54 +0000

    add url path for form in contact form

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.DetailBlog.as_view(), name='detail_blog'),
-    path('', views.ContactFormView.as_view(), name='contact'),
+    path('contact/', views.ContactFormView.as_view(), name='contact'),
+    path('<slug:slug>/', views.DetailBlog.as_view(), name='detail_blog'), 
 ]
