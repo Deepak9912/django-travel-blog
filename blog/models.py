9af12aa0c852a0b50e2f95a8a7b48fe0268f2346
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-21 21:11:54 +0000

    add migrations

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,11 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    body = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now=True)
