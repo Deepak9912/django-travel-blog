f4311cae3d11fede6931f61eec5ddf68147a538c
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-06-27 22:17:24 +0000

    update view for detail blog

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,6 @@ class DetailBlog(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
 
         return render(
             request,
@@ -29,7 +28,6 @@ class DetailBlog(View):
                 "post": post,
                 "comments": comments,
                 "commented": False,
-                "liked": liked,
                 "comment_form": CommentForm()
             },
         )
