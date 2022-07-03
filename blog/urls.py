230d72a235d174d20a66c6aae1af4391f2a5f781
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-07-03 17:21:01 +0000

    delete url for contact page

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('', views.contact.as_view(), name='contact'),
     path('delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
     path('edit_comment/<int:pk>', views.EditComment.as_view(), name='edit_comment'),
     path('<slug:slug>/', views.DetailBlog.as_view(), name='detail_blog'),  
