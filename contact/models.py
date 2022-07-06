fddba5a1eeee2c69a6522d83f52d9c1df618eb49
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-07-06 20:22:16 +0000

    create a model for contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+    subject = models.CharField(max_length=300)
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(Self):
+        return self.email
