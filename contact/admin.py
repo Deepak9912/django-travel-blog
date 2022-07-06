8eaf8b67a2beb798376d0538ea27cf2fcee29cac
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-07-06 21:13:39 +0000

    update email field in settings.py

--- contact/admin.py
+++ contact/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Contact
 
-@admin.register(Contact)
+
 class ContactAdmin(admin.ModelAdmin):
     list_display= ('name', 'email', 'subject', 'created_on', 'updated_on')
 
+admin.site.register(Contact, ContactAdmin)
