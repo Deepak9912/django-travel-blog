072f5995441b08c4ba7180e15d44e369a325971c
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-07-07 19:47:22 +0000

    update the default from email

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -170,7 +170,7 @@ DEFAULT_FROM_EMAIL = 'explorer@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'explorer@example.com'
+    DEFAULT_FROM_EMAIL = 'deepaksinghj72@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
