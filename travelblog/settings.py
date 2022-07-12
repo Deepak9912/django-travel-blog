0f686bf071dfa9fd9498d2c8045bdc4acb65cca4
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-07-12 15:46:14 +0000

    update cloudinary file

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -143,13 +143,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # media files
 
