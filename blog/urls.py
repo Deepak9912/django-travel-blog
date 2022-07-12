b52f5ec9306614703218957821b150b8e530b3dc
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-07-12 15:05:03 +0000

    update static file settings

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
     path('edit_comment/<int:pk>', views.EditComment.as_view(), name='edit_comment'),
     path('<slug:slug>/', views.DetailBlog.as_view(), name='detail_blog'),  
-    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
